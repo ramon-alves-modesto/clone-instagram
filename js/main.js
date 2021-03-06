1e84b3f1b099f40129a1f21830f0b56c369a0b31
Ramon9825 <ramonmodesto16@protonmail.com>
2021-03-06 01:16:30 -0300

    Atualização

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function on(elemento) {
+    if(elemento.checked) {
+        document.getElementById('corpo').style.backgroundColor = '#FFF';
+        let grupos = document.querySelectorAll('.grupo'); 
+        for(let i = 0; i < grupos.length; i++) {
+            grupos[i].style.backgroundColor = '#FFF';
+        }
+
+        let botoes = document.querySelectorAll('.botao');
+        for(let i = 0; i < botoes.length - 1; i++) {
+            botoes[i].style.border = 'thin solid #555';
+        }
+        document.body.style.color = '#000';
+        document.getElementById('logo').src = "img/instagram-logo2.png";
+        document.getElementById('esqueceu').style.color = '#000';
+    } else {
+        document.getElementById('corpo').style.backgroundColor = '#000';
+        let grupos = document.querySelectorAll('.grupo');
+        for(let i = 0; i < grupos.length; i++) {
+            grupos[i].style.backgroundColor = '#000';
+        }
+        document.body.style.color = '#FFF';
+        document.getElementById('logo').src = "img/instagram-logo.png";
+        document.getElementById('esqueceu').style.color = '#FFF';
+    }
+
+}
+
+
